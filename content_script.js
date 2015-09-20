967e53b91ea9d3cd84f0d9eacc7eb35c15564ecb
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2015-09-21 03:02:37 +0900

    ↓へドラッグで検索、→へドラッグでコピー機能実装

---
+++ content_script.js
@@ -0,0 +1,35 @@
+﻿
+var slectedText = null;
+var startPositionX = 0;
+var startPositionY = 0;
+
+document.body.addEventListener("dragstart", function (evt) {
+	slectedText = window.getSelection().toString();
+	startPositionX = evt.screenX;
+	startPositionY = evt.screenY;
+});
+
+document.body.addEventListener("dragend", function (evt) {
+	if (slectedText === window.getSelection().toString()) {
+		var movedDistanceToRight = evt.screenX - startPositionX;
+		var movedDistanceToBottom = evt.screenY - startPositionY;
+		if (movedDistanceToBottom > 0) {
+			// 下へ動いた
+			if ((movedDistanceToRight > 0) && (movedDistanceToRight > movedDistanceToBottom)) {
+				// 右へ動いた かつ 右の方へ大きく動いた
+				send("copy", slectedText);
+			} else {
+				send("search", slectedText);
+			}
+		}
+	}
+});
+
+function send(method, text) {
+	chrome.runtime.sendMessage({
+		method: method,
+		text: text
+	}, function (response) {
+		
+	});
+}
